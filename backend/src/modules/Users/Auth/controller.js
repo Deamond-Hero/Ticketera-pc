5360fd5131cdfc43ebd4095683602bb1ea906c44
FerTorron <fertorron21@gmail.com>
2024-05-20 19:27:03 -0300

    Muevo el Register a Auth

--- backend/src/modules/Users/Auth/controller.js
+++ backend/src/modules/Users/Auth/controller.js
@@ -1,15 +1,23 @@
-import { resSuccess, resFail } from "../../config/utils/response.js";
-import { logger } from "../../config/logger.js";
+import { resSuccess, resFail } from "../../../config/utils/response.js";
+import { createUserService } from "./services.js";
+import { logger } from "../../../config/logger.js";
 // Importar service
 
 export const login = async (req, res) => {
-  
+
 };
 
 export const register = async (req, res) => {
-  
+    try {
+        const { email, password } = req.body;
+        const result = await createUserService({ email, password });
+        resSuccess(res, 200, "User created successfully", result);
+    } catch (error) {
+        logger.error(error);
+        resFail(res, 400, error.message, error);
+    }
 };
 
 export const logout = async (req, res) => {
-  
+
 };
--- backend/src/modules/Users/controller.js
+++ backend/src/modules/Users/controller.js
@@ -1,20 +1,10 @@
 import { resSuccess, resFail } from "../../config/utils/response.js";
 import { logger } from "../../config/logger.js";
-import { createUserService } from "./services.js";
 
-export const createUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const result = await createUserService({ email, password });
-    resSuccess(res, 200, "User created successfully", result);
-  } catch (error) {
-    logger.error(error);
-    resFail(res, 400, error.message, error);
-  }
-};
+export const createUser = async (req, res) => { };
 
-export const getUser = async (req, res) => {};
+export const getUser = async (req, res) => { };
 
-export const updateUser = async (req, res) => {};
+export const updateUser = async (req, res) => { };
 
-export const deleteUser = async (req, res) => {};
+export const deleteUser = async (req, res) => { };
