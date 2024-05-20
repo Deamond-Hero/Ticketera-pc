4de6e69c4e1cf1dcbfde935b81249b285b61e02b
Nicolas Ippoliti <91435048+NicolasIppoliti@users.noreply.github.com>
2024-05-20 19:20:08 -0300

    KAN-12 login

--- backend/src/modules/Users/Auth/controller.js
+++ backend/src/modules/Users/Auth/controller.js
@@ -1,9 +1,15 @@
-import { resSuccess, resFail } from "../../config/utils/response.js";
+import { resSuccess, resFail } from "../../../config/utils/response.js";
 import { logger } from "../../config/logger.js";
-// Importar service
+import { loginService } from "./services.js";
 
 export const login = async (req, res) => {
-  
+  try {
+    const result = await loginService(req.body);
+    resSuccess(res, 200, "Inicio de sesion exitoso", result);
+  } catch (error) {
+    logger.error(error);
+    resFail(res, 400, "Inicio de sesion fallido", error);
+  }
 };
 
 export const register = async (req, res) => {
--- backend/src/modules/Users/Auth/services.js
+++ backend/src/modules/Users/Auth/services.js
@@ -1,2 +1,19 @@
 import { logger } from "../../config/logger.js";
 import User from "../schema.js";
+import { isValidPassword } from "../../../config/utils/hash.js";
+
+export const loginService = async (email, password) => {
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("Usuario no encontrado");
+    }
+    if (!isValidPassword(user, password)) {
+      throw new Error("Contraseña incorrecta");
+    }
+    return user;
+  } catch (error) {
+    logger.error("Error al iniciar sesión: " + error);
+    throw error;
+  }
+};
