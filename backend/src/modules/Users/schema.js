5dafc1f9bef28d07e79501478657390676024e02
Nicolas Ippoliti <91435048+NicolasIppoliti@users.noreply.github.com>
2024-05-23 16:37:08 -0300

    User schema fix, tickets [] added

--- backend/src/modules/Users/schema.js
+++ backend/src/modules/Users/schema.js
@@ -5,6 +5,7 @@ const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, unique: true },
   role: { type: String, required: true, enum: ["Cliente", "Tecnico", "Administrador"], default: "Cliente" },
+  tickets: [{ type: Schema.Types.ObjectId, ref: "Ticket", default: [] }],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
