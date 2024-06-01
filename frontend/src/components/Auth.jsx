2e6a837de759fe772a5f93a1842cac84388fdc1c
LeandroBrangi <leandro.brangi@gmail.com>
2024-05-31 22:10:45 -0300

    Login validaciones

--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -4,54 +4,45 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
 import { LoginService } from "../redux/actionsUser";
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect } from "react"
-import api from "../utils/Api";
+import { validateLogin } from "../utils/validationLogin";
 
 const Auth = () => {
 
     const dispatch = useDispatch()
-    const dataUser = useSelector(state => state.dataUser)
+    const dataUser = useSelector(state => state.auth.userData)
+    const errorRequest = useSelector(state => state.auth.userMesaggeError)
     const navigate = useNavigate()
-    useEffect(() => {
-
-        console.log(dataUser)
-
-    }, [dataUser])
-
+    const [flag, setFlag] = useState(false)
     const [form, setForm] = useState({
         email: '',
         password: '',
     });
-
     const [errors, setErrors] = useState({
         email: '',
         password: '',
     });
 
-    const validate = () => {
-        const errors = { email: '', password: '' };
 
-        if (!form.email) {
-            errors.email = "El email es obligatorio";
-        } else if (!/\S+@\S+\.\S+/.test(form.email)) {
-            errors.email = "El email no es válido";
+    useEffect(() => {
+        if (errorRequest) {
+            setFlag(true)
+            setTimeout(() => {
+                setFlag(false)
+            }, 3000);
+            setFlag(false)
         }
+    }, [errorRequest])
 
-        if (!form.password) {
-            errors.password = "La contraseña es obligatoria";
-        } else if (form.password.length < 6) {
-            errors.password = "La contraseña debe tener al menos 6 caracteres";
-
-        } else if (!/[a-z]/.test(form.password)) {
-            errors.password = "La contraseña debe tener al menos una letra minúscula";
-        } else if (!/[0-9]/.test(form.password)) {
-            errors.password = "La contraseña debe tener al menos un número";
-
+    useEffect(() => {
+        if (errors.email || errors.password) {
+          setFlag(true);
+          const timer = setTimeout(() => {
+            setFlag(false);
+          }, 3000);
+    
+          return () => clearTimeout(timer);
         }
-
-
-        return errors;
-    };
-
+      }, [errors]);
 
 
     const valueChange = (e) => {
@@ -64,7 +55,7 @@ const Auth = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const validationErrors = validate();
+        const validationErrors = validateLogin();
         setErrors(validationErrors);
 
         if (!validationErrors.email && !validationErrors.password) {
@@ -76,44 +67,46 @@ const Auth = () => {
 
             console.log("Formulario enviado", form);
         } else {
-            // Hay errores, no se envía el formulario
+
             console.log("Errores en el formulario", validationErrors);
 
         }
     };
 
     return (
-        <div className="bg-[#FFFFFF] text-text-dark w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-12">
-            <form onSubmit={handleSubmit} className="flex flex-col gap-6">
-                <h1 className="font-semibold text-5xl tracking-wide mb-6">Iniciar sesión</h1>
-                <div className="flex flex-col gap-1">
+        <div>
+            <h1>Iniciar sesión</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
                     <label>Email</label>
                     <input
                         type="email"
                         value={form.email}
                         name="email"
                         onChange={valueChange}
-                        className="rounded-lg bg-[#FBFBFB] h-12 w-96 border border-text-dark text-text-dark px-2  focus:shadow-input-focus focus:outline-none focus:border-none"
                     />
-                    {errors.email && <span style={{ color: "red" }}>{errors.email}</span>}
                 </div>
-                <div className="flex flex-col gap-1">
+
+                {flag && errors.email && <span style={{ color: "red" }}>{errors.email}</span>}
+
+                <div>
                     <label>Contraseña</label>
                     <input
                         type="password"
                         value={form.password}
                         name="password"
                         onChange={valueChange}
-                        className="rounded-lg bg-[#FBFBFB] h-12 w-96 border border-text-dark text-text-dark px-2  focus:shadow-input-focus focus:outline-none focus:border-none"
                     />
-                    {errors.password && <span style={{ color: "red" }}>{errors.password}</span>}
                 </div>
+
+                {flag && errors.password && <span style={{ color: "red" }}>{errors.password}</span>}
+
                 <div>
-                    <button type="submit" className="h-12 w-96 rounded-lg text-[#FFFFFF] text-xl tracking-wide bg-default-btn">Ingresar</button>
+                    <button type="submit">Ingresar</button>
                 </div>
             </form>
             <div>
-                <Link><p className="text-xl underline tracking-wide">¿Has olvidado tu contraseña?</p></Link>
+                <Link><p>¿Has olvidado tu contraseña?</p></Link>
             </div>
         </div>
     );
