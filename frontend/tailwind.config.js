b94960cebd6242bbcd7fe2e7c2d3590bbb33ba82
Nicofontanini <anicolasfontanini@gmail.com>
2024-06-05 19:24:05 -0300

    Guardar cambios locales antes de fusionar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,7 +12,13 @@ export default {
         'shadow-input': '#00A4E8',
         'text-dark': '#303030',
         'text-light': '#808080',
+        'principal': '#004562',
+        'color-input': '#979797',
       },   
+      backgroundImage: {
+        'gradient-custom': 'linear-gradient(to right, #004562, #42B4C2)',
+        
+      },
       boxShadow: {
         'input-focus': '0px 0px 4px .1px #00A4E8',
       }, 
